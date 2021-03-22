2b8c8a1b492c23fb70ac393ce65bd39131d72d1c
sunxilin <656998813@qq.com>
2021-03-22 20:14:20 +0800

    daily commit 0322

--- app/src/main/java/com/njupt/multibiometricauth/Constants.java
+++ app/src/main/java/com/njupt/multibiometricauth/Constants.java
@@ -9,4 +9,13 @@ public class Constants {
 
     public static String ArcSoftAppId = "AFauwf5FP3Ako93oRKH6LTPRFW1szy8r4TpzPWvTb9p";
     public static String ArcSoft_SDK_KEY = "B5ake7FMMpy6q5UYo76ncns5xsvCqDSJi1kF6qZmQqb5";
+
+    /**
+     * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int HORIZONTAL_OFFSET = 0;
+    /**
+     * IR预览数据相对于RGB预览数据的纵向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int VERTICAL_OFFSET = 0;
 }
---
+++ app/src/main/java/com/njupt/multibiometricauth/face/util/face/RequestLivenessStatus.java
@@ -0,0 +1,5 @@
+package com.njupt.multibiometricauth.face.util.face;
+
+public class RequestLivenessStatus {
+    public static final int ANALYZING = 10;
+}
